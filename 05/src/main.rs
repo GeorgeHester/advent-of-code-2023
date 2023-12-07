647d5782fd65b6ecbfc1c0d98670082984f6555a
George Hester <48884343+GeorgeHester@users.noreply.github.com>
2023-12-07 05:54:35 +0000

    perf(day 5): implemented hash maps

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections;
 use std::fs;
 use std::io;
 use std::path;
@@ -10,13 +11,7 @@ enum Section {
 enum LineOperation {
     Skip,
     Operate,
-    SkipNewLevel,
-}
-
-struct Range {
-    input_start: u64,
-    input_end: u64,
-    output_start: u64,
+    NewLevel,
 }
 
 fn read_file<P: AsRef<path::Path>>(file_name: P) -> io::Lines<io::BufReader<fs::File>> {
@@ -26,6 +21,18 @@ fn read_file<P: AsRef<path::Path>>(file_name: P) -> io::Lines<io::BufReader<fs::
     };
 
     return io::BufRead::lines(io::BufReader::new(file));
+}
+
+fn determine_operation(input: &String) -> LineOperation {
+    if input.len() == 0 {
+        return LineOperation::NewLevel;
+    }
+
+    if input.contains("map") {
+        return LineOperation::Skip;
+    }
+
+    return LineOperation::Operate;
 }
 
 fn parse_seed_ids(input: String) -> Vec<u64> {
@@ -46,77 +53,38 @@ fn parse_seed_ids(input: String) -> Vec<u64> {
     return output;
 }
 
-fn parse_range(input: String) -> Range {
+fn update_range_map(map: &mut collections::HashMap<u64, u64>, input: &String) {
     let values: Vec<u64> = input
         .trim()
         .split_whitespace()
         .map(|value| value.parse::<u64>().unwrap())
         .collect();
 
-    return Range {
-        input_start: values[1],
-        input_end: values[1] + values[2] - 1,
-        output_start: values[0],
-    };
+    for input_value in values[1]..values[1] + values[2] {
+        let output_value = values[0] + input_value - values[1];
+        map.insert(input_value, output_value);
+    }
 }
 
-fn determine_operation(input: &String) -> LineOperation {
-    if input.len() == 0 {
-        return LineOperation::SkipNewLevel;
-    }
+fn determine_last_level(inital: u64, maps: &Vec<collections::HashMap<u64, u64>>) -> u64 {
+    let mut previous_map_value: u64 = inital;
 
-    if input.contains("map") {
-        return LineOperation::Skip;
-    }
-
-    return LineOperation::Operate;
-}
-
-fn print_levels(levels: Vec<Vec<Range>>) {
-    let mut level_index = 0;
-    for level in levels {
-        for range in level {
-            println!(
-                "{} {} {} {}",
-                level_index, range.input_start, range.input_end, range.output_start
-            );
+    for map_index in 0..maps.len() {
+        if maps[map_index].contains_key(&previous_map_value) {
+            previous_map_value = maps[map_index][&previous_map_value];
         }
-        level_index += 1;
-    }
-}
-
-fn calculate_range_output(input: u64, range: &Range) -> u64 {
-    return range.output_start + (input - range.input_start);
-}
-
-fn determine_range(input: u64, ranges: &Vec<Range>) -> u64 {
-    for range in ranges {
-        if input < range.input_start || input > range.input_end {
-            continue;
-        }
-
-        return calculate_range_output(input, &range);
     }
 
-    return input;
-}
-
-fn determine_last_level(start_value: u64, levels: &Vec<Vec<Range>>) -> u64 {
-    let mut previous_level_value: u64 = start_value;
-    for level_index in 0..levels.len() {
-        previous_level_value = determine_range(previous_level_value, &levels[level_index]);
-    }
-
-    return previous_level_value;
+    return previous_map_value;
 }
 
 fn main() {
     let lines = read_file("input.txt");
 
-    let mut seeds: Vec<u64> = Vec::new();
     let mut section = Section::Seed;
-    let mut levels: Vec<Vec<Range>> = Vec::new();
-    let mut level: Vec<Range> = Vec::new();
+    let mut seeds: Vec<u64> = Vec::new();
+    let mut level_maps: Vec<collections::HashMap<u64, u64>> = Vec::new();
+    let mut level_map: collections::HashMap<u64, u64> = collections::HashMap::new();
 
     for line_result in lines {
         let line = line_result.unwrap();
@@ -128,34 +96,31 @@ fn main() {
                     continue;
                 }
 
-                let seed_ids = parse_seed_ids(line);
-
-                for seed_id in seed_ids {
-                    seeds.push(seed_id);
-                }
+                seeds = parse_seed_ids(line);
+                seeds.dedup();
             }
             Section::Map => match determine_operation(&line) {
-                LineOperation::SkipNewLevel => {
-                    levels.push(level);
-                    level = Vec::new();
+                LineOperation::NewLevel => {
+                    level_maps.push(level_map);
+                    level_map = collections::HashMap::new();
                     continue;
                 }
                 LineOperation::Skip => {
                     continue;
                 }
                 LineOperation::Operate => {
-                    level.push(parse_range(line));
+                    update_range_map(&mut level_map, &line);
                 }
             },
         }
     }
 
-    levels.push(level);
+    level_maps.push(level_map);
 
     let mut lowest_location: u64 = u64::MAX;
 
     for seed in seeds {
-        let new_location: u64 = determine_last_level(seed, &levels);
+        let new_location: u64 = determine_last_level(seed, &level_maps);
 
         if lowest_location > new_location {
             lowest_location = new_location
